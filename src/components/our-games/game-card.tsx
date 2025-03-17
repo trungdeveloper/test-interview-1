86ee8d388a275954f58e9125b3b9109f8ba64e7a
Barry Nguyen <trung.nguyentan@orientsoftware.com>
2025-03-17 23:21:24 +0700

    fix link image

--- src/components/our-games/game-card.tsx
+++ src/components/our-games/game-card.tsx
@@ -11,7 +11,7 @@ export default function GameCard({ title, image, description }: GameCardProps) {
     <Card className="relative overflow-hidden rounded-2xl w-full transition-transform hover:scale-[1.02] min-w-41 h-67 max-w-[410px] lg:h-140 ">
       <div
         className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: `url(src/${image})` }}
+        style={{ backgroundImage: `url(${image})` }}
       />
       <div className="absolute bottom-0 left-0 p-4 text-white font-montserrat">
         <h3 className="font-bold text-2xl leading-[1.88rem] align-bottom mb-2 lg:text-5xl lg:leading-[3.75rem]">
